13013097f4b21269566ef971683b1d56a14ba8c9
Trịnh Như Ý <31103455+tnymu96@users.noreply.github.com>
2023-05-31 17:20:33 +0700

    first commit

---
+++ src/pages/Login.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../styles/images/logo-shopee.png';
+
+const Login = () => {
+    return (
+        <>
+            <div>
+                <div className='d-flex align-items-center'>
+                    <img src={logo} alt='Shopee' height={50} className='p-2' />
+                    <div className='p-2 fw-normal fs-3'>Đăng nhập</div>
+                    <div className='ms-auto p-2'>Bạn cần giúp đỡ?</div>
+                </div>
+            </div >
+        </>
+    )
+}
+
+export default Login;
